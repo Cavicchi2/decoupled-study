a0dd3c2e9ce3674d88f3d2fc98d807fef270f506
Cavicchi2 <caiocavicchia@gmail.com>
2023-01-25 17:08:14 -0300

    Letting react get node types dinamically

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -9,12 +9,14 @@ function App() {
     <section className="App main-section flex flex-col">
       <Header />
       <div className='text-center text-lg py-5'>You can edit nodes in Drupal site and they will be updated here.</div>
-      <div className='main-section bg-gray-200 pl-10 pr-10 flex flex-row m-auto'>
+      <div className='main-section bg-gray-200 pl-10 pr-10 flex flex-row gap-x-10 m-auto'>
         <NodeBlock type='article'> 
           <NodeList type='article' />
         </NodeBlock>
+        <NodeBlock type='page'>
+          <NodeList type='page' />
+        </NodeBlock>
       </div>
-      <Footer />
     </section>
   );
 }
