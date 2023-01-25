a30af6b539e9906a5eceea6fed1c293d5f3fbecc
Cavicchi2 <caiocavicchia@gmail.com>
2023-01-25 17:40:20 -0300

    Fixing not having services for cors

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,10 +4,20 @@ import Footer from './Components/Footer';
 import NodeBlock from './Components/Node/NodeBlock';
 import NodeList from './Components/Node/NodeList';
 
+// Function to clear complete cache data
+const clearCacheData = () => {
+    caches.keys().then((names) => {
+        names.forEach((name) => {
+            caches.delete(name);
+        });
+    });
+    alert('Complete Cache Cleared')
+};
 function App() {
   return (
     <section className="App main-section flex flex-col">
       <Header />
+      <button onClick={() => clearCacheData()} >Clear Cache Data</button>
       <div className='text-center text-lg py-5'>You can edit nodes in Drupal site and they will be updated here.</div>
       <div className='main-section bg-gray-200 pl-10 pr-10 flex flex-row gap-x-10 m-auto'>
         <NodeBlock type='article'> 
@@ -17,6 +27,7 @@ function App() {
           <NodeList type='page' />
         </NodeBlock>
       </div>
+      <Footer />
     </section>
   );
 }
